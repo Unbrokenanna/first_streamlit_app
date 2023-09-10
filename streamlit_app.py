0abed59d3ec86bd6ff97ad1b069f5cadde78ce1f
Unbrokenanna <88595408+Unbrokenanna@users.noreply.github.com>
2023-09-10 22:50:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ def get_fruityvice_data(this_fruit_choice):
 def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
            my_cur.execute("SELECT * from fruit_load_list")
-          return my_cur.fetchall()
+     return my_cur.fetchall()
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header('Breakfast Favorites')
